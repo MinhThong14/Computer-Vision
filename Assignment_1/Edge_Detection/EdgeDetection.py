f84acd2130bb8e1c10066ffbd8f92380ab20cb8d
MinhThong14 <achilles.mai14@gmail.com>
2023-02-11 16:36:16 -0500

    Re-organized edge dection folder

--- Assignment_1/Edge_Detection/EdgeDetection.py
+++ Assignment_1/Edge_Detection/EdgeDetection.py
@@ -4,16 +4,32 @@ import matplotlib.pyplot as plt
 import math
 import copy
 
-# Smoothing image
+INPUT_PATH = "./Input_Image"
+OUTPUT_PATH = "./Output_Image"
+
 def gaussian_blur(img0, sigma):
+    """
+    Function that smoothing the image using gaussian blur
+
+    :param img0: input image
+    :param sigma: standard deviation
+    :return: the smoothing image after applying gaussian kernel
+
+    """
     # hsize
     hsize = 2 * int(math.ceil(3 * sigma)) + 1
     # gaussian blur
     img0 = cv2.GaussianBlur(img0, (hsize, hsize), sigma)
     return img0
 
-# Sobel filters
 def sobel_filters(img0):
+    """
+    Function that create sobel filters
+
+    :params img0: input image
+    :return image gradients in the x and y direction
+
+    """
     # Sobel filter for gradient in x direction
     sobel_x = np.array([[-1, 0, 1], [-2, 0, 2], [-1, 0, 1]])
     imgx = cv2.filter2D(img0, cv2.CV_64F, sobel_x)
@@ -24,18 +40,52 @@ def sobel_filters(img0):
     
     return imgx, imgy
 
-# Calculate gradient magnitude and orientation
 def gradient_magnitude(imgx, imgy):
+    """
+    Function that creates the gradient magnitude image
+
+    :param imgx: image gradient in the x direction
+    :param imgy: image gradient in the y direction
+    :return gradient magnitude image
+    """
     img1 = np.sqrt(np.power(imgx, 2) + np.power(imgy, 2))
     return img1
 
-# Calculate gradient magnitude and orientation
 def gradient_orientation(imgx, imgy):
+    """
+    Fuction that createas gradient orientation image
+
+    :param imgx: image gradient in the x direction
+    :param imgy: image gradient in the y direction
+    :return gradient oriretation image
+
+    """
     orientation = np.arctan2(imgy, imgx) * 180 / np.pi
     return orientation
 
-# Edge filter using non-maximum suppression
+def apply_threshold(img1):
+    """
+
+    Function that apply a simple threshold to image
+
+    :param img1: a grayscale image
+    :return the image that has pixels above the threshold
+    
+    """
+    threshold = 30  
+    img1[img1 < threshold] = 0
+    img1[img1 >= threshold] = 255
+    return img1
+
 def my_edge_filter(img0, sigma):
+    """
+    Function that dectect the edge of an image
+
+    :param img0: a gray scale image
+    :param sigma: scalar (standard deviation of the Gaussian smoothing kernel)
+    :return the the edge magnitude image
+
+    """
     # Smoothing image
     img0 = gaussian_blur(img0, sigma)
 
@@ -68,44 +118,45 @@ def my_edge_filter(img0, sigma):
                 if (img1[i, j] < img1[i-1, j-1]) or (img1[i, j] < img1[i+1, j+1]):
                     result[i, j] = 0
     
-    return result
+    return apply_threshold(result)
 
-# Edge filter with threshold 
-def my_edge_filter_with_threshold(img1):
-    # Thresholding
-    threshold = np.max(img1) * 0.1
-    img1[img1 < threshold] = 0
-    return img1
 
 if __name__ == "__main__":
-    # Import image
-    img = cv2.imread("./cat2.jpg", 0)
-    # # Gaussian blur image
-    # gaussian_blur_img = gaussian_blur(img, sigma=0)
-    
-    # # Sobels
-    # imgx, imgy = sobel_filters(gaussian_blur_img)
+    # Input images
+    input_images = ["cat2.jpg", "img0.jpg", "littledog.jpg"]
 
-    # # Gradient magnitude image
-    # gradient_magnitude_img = gradient_magnitude(imgx, imgy)
+    # Genetate edge dection for all input images
+    for input_img in input_images:
+        input = INPUT_PATH + '/' + input_img
+        # Import image
+        img = cv2.imread(input,0)
+        # Gaussian blur image
+        gaussian_blur_img = gaussian_blur(img, sigma=0)
+        
+        # Sobels
+        imgx, imgy = sobel_filters(gaussian_blur_img)
 
-    # # Gradient orientation image 
-    # gradient_orientation_img = gradient_orientation(imgx, imgy)
-    
-    # Non_maximum image
-    non_maximum_img = my_edge_filter(img, sigma=2)
-    
-    # Save images
-    # gradient_magnitude_img = np.uint8(gradient_magnitude_img)
-    # gradient_magnitude_img = cv2.normalize(gradient_magnitude_img, None, 0, 255, cv2.NORM_MINMAX, cv2.CV_8U)
-    # cv2.imwrite('Gradient_Magnitude.jpg', gradient_magnitude_img) 
-    # cv2.waitKey(0)
+        # Gradient magnitude image
+        gradient_magnitude_img = gradient_magnitude(imgx, imgy)
 
-    # gradient_orientation_img = cv2.convertScaleAbs(gradient_orientation_img)
-    # cv2.imwrite('Gradient_Orientation.jpg', gradient_orientation_img)
-    # cv2.waitKey(0)
-    
-    # non_maximum_img = cv2.cvtColor(non_maximum_img, cv2.COLOR_BGR2GRAY)
-    # non_maximum_img = cv2.normalize(non_maximum_img, None, 0, 255, cv2.NORM_MINMAX, cv2.CV_8U)
-    cv2.waitKey(0)
-    
+        # Gradient orientation image 
+        gradient_orientation_img = gradient_orientation(imgx, imgy)
+        
+        # Non_maximum image
+        non_maximum_img = my_edge_filter(img, sigma=2)
+
+        # Image name
+        img_name = input_img.split('.')[0]
+
+        # Save images
+        gradient_magnitude_img = np.uint8(gradient_magnitude_img)
+        gradient_magnitude_img = cv2.normalize(gradient_magnitude_img, None, 0, 255, cv2.NORM_MINMAX, cv2.CV_8U)
+        output_magnitude_img = OUTPUT_PATH + '/' + img_name + '/' + img_name + '_gradient_magnitude.jpg'
+        cv2.imwrite(output_magnitude_img , gradient_magnitude_img) 
+
+        gradient_orientation_img = cv2.convertScaleAbs(gradient_orientation_img)
+        output_orientaion_img = OUTPUT_PATH + '/' + img_name + '/' + img_name + '_gradient_orientation.jpg'
+        cv2.imwrite(output_orientaion_img, gradient_orientation_img)
+        
+        output_non_maximum_img = OUTPUT_PATH + '/' + img_name + '/' + img_name + '_non_maximum.jpg'
+        cv2.imwrite(output_non_maximum_img, non_maximum_img)
